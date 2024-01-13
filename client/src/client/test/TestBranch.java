f317a5ddb1fe05c05d044fc698afb40d3ff100af
sansan1307 <121425928+sansan1307@users.noreply.github.com>
2024-01-13 21:39:58 +0800

    add java

--- client/src/client/test/TestBranch.java
+++ client/src/client/test/TestBranch.java
@@ -11,6 +11,7 @@ package client.test;
  */
 public class TestBranch {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello Java");
+       
     }
 }
